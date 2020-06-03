54e63296611ca88c2fc11a9f7ed2fbe97b672da8
Jose Enciso <josejefloen@gmail.com>
2020-06-03 23:10:49 +0000

    Creating Login route and function

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 import os
+import pymysql
 # import json
 from flask import Flask, render_template
 
+
+username = os.getenv('C9_USER')
+
+# Connnect to DB
+# connection = pymysql.connect(host='localhost', user=username, password='', db='review_post')
 
 app = Flask(__name__)
 
